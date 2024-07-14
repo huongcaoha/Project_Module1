7011dfb103984016f8a5712f9f5b06bb668334dd
huongcaoha <huongcaoha1994@gmail.com>
2024-07-14 10:16:34 +0700

    update code

--- javascript/admin.js
+++ javascript/admin.js
@@ -1,3 +1,55 @@
+// Controls dashbroad
+const controlContent1 = document.getElementById("content1");
+controlContent1.addEventListener("click", function () {
+  updateDataLocalStorage("currentDisplayContent", 1);
+  window.location.reload();
+});
+console.log(controlContent1);
+const controlProduct = document.getElementById("section-product");
+controlProduct.addEventListener("click", function () {
+  updateDataLocalStorage("currentDisplayContent", 2);
+  window.location.reload();
+});
+
+const controlRevenue = document.getElementById("section-revenue");
+controlRevenue.addEventListener("click", function () {
+  updateDataLocalStorage("currentDisplayContent", 3);
+  window.location.reload();
+});
+
+const controlUser = document.getElementById("section-user");
+controlUser.addEventListener("click", function () {
+  updateDataLocalStorage("currentDisplayContent", 4);
+  window.location.reload();
+});
+
+const controlOrder = document.getElementById("section-order");
+controlOrder.addEventListener("click", function () {
+  updateDataLocalStorage("currentDisplayContent", 5);
+  window.location.reload();
+});
+
+let content1 = document.querySelector(".content1");
+let sectionProduct = document.querySelector(".section-product");
+let sectionRevenue = document.querySelector(".section-revenue");
+let sectionUser = document.querySelector(".section-user");
+let sectionOrder = document.querySelector(".section-order");
+console.log(content1);
+let listControls = [
+  "123",
+  content1,
+  sectionProduct,
+  sectionRevenue,
+  sectionUser,
+  sectionOrder,
+];
+let currentDisplayContent = getDataLocalstorage("currentDisplayContent");
+for (let i = 0; i < listControls.length; i++) {
+  if (i != currentDisplayContent) {
+    listControls[i].classList.add("display-none");
+  }
+}
+
 // section products
 function getDataLocalstorage(nameData) {
   return JSON.parse(localStorage.getItem(nameData));
@@ -9,16 +61,19 @@ function updateDataLocalStorage(nameData, newData) {
 
 let tagTableProduct = document.getElementById("table-product");
 
-let totalProducts = Object.values(getDataLocalstorage("products"));
+let totalProducts = [];
+let productsData = getDataLocalstorage("products");
+if (productsData == null) {
+} else {
+  totalProducts = Object.values(getDataLocalstorage("products"));
+}
 let productTagPagination = document.querySelector(".pagination");
 const itemPerPage = 5;
 let totalPage = Math.ceil(totalProducts.length / itemPerPage);
-let adminProductCurrentPage = 0;
+let adminProductCurrentPage = 1;
 
 if (getDataLocalstorage("adminProductCurrentPage")) {
   adminProductCurrentPage = getDataLocalstorage("adminProductCurrentPage");
-} else {
-  adminProductCurrentPage = 1;
 }
 let skipProduct = (adminProductCurrentPage - 1) * itemPerPage;
 let currentProducts = totalProducts.slice(
@@ -111,7 +166,7 @@ function getCurrentPagination(adminProductCurrentPage) {
   return rs;
 }
 
-function updateProduct(
+function updateProducts(
   tagTableProduct,
   tagPagination,
   adminProductCurrentPage,
@@ -120,7 +175,7 @@ function updateProduct(
   tagTableProduct.innerHTML += getProduct(currentProducts);
   tagPagination.innerHTML += getCurrentPagination(adminProductCurrentPage);
 }
-updateProduct(
+updateProducts(
   tagTableProduct,
   productTagPagination,
   adminProductCurrentPage,
@@ -198,10 +253,15 @@ function createNewProduct() {
   let productInputImage2 = document.getElementById("productInputImage2");
 
   //còn thiếu đoạn code lưu ảnh
-  let imageName1 = productInputImage1.files[0].name;
-  let imageName2 = productInputImage2.files[0].name;
+  let imageName1 = productInputImage1?.files?.[0]?.name ?? "";
 
-  let totalProducts = getDataLocalstorage("products");
+  let imageName2 = productInputImage2?.files?.[0]?.name ?? "";
+
+  let totalProducts = [];
+  const productsData = getDataLocalstorage("products");
+  if (productsData != null) {
+    totalProducts = Object.values(getDataLocalstorage("products"));
+  }
   let productName = productInputName.value;
   let price = productInputPrice.value;
   let color = productInputColor.value;
@@ -209,7 +269,10 @@ function createNewProduct() {
   let inventory = productInputInventory.value;
   let image1 = imageName1;
   let image2 = imageName2;
-  let id = totalProducts[totalProducts.length - 1].id + 1;
+  let id = 1;
+  if (totalProducts.length > 1 && totalProducts != null) {
+    id = totalProducts[totalProducts.length - 1].id + 1;
+  }
 
   let newProduct = {
     id: id,
@@ -233,6 +296,7 @@ let productButtonUpdate = document.getElementById("productButtonUpdate");
 let listButtonUpdate = document.querySelectorAll(".productButtonUpdate");
 
 // kiểm tra button cancel2 có tồn tại ko?
+let productButtonCancel2 = document.querySelector(".productButtonCancel2");
 
 for (let btnUpdate of listButtonUpdate) {
   btnUpdate.addEventListener("click", function () {
@@ -246,25 +310,25 @@ for (let btnUpdate of listButtonUpdate) {
         </div>
 
         <div>
-          <label for="productInputName">Product Name :</label>
-          <input type="text" id="productInputName" class="productInputName" value="${
+          <label for="productInputName2">Product Name :</label>
+          <input type="text" id="productInputName2" class="productInputName2" value="${
             product.name
           }"/>
         </div>
 
         <div>
-          <label for="productInputPrice">Price : </label>
+          <label for="productInputPrice2">Price : </label>
           <input
             type="number"
-            id="productInputPrice"
-            name="productInputPrice"
+            id="productInputPrice2"
+            name="productInputPrice2"
             value="${product.price}"
           />
         </div>
 
         <div>
-          <label for="productInputColor">Color :</label>
-          <select name="productInputColor" id="productInputColor">
+          <label for="productInputColor2">Color :</label>
+          <select name="productInputColor2" id="productInputColor2">
             <option value="">Select</option>
             <option value="black" ${
               product.color === "black" ? "selected" : ""
@@ -294,8 +358,8 @@ for (let btnUpdate of listButtonUpdate) {
         </div>
 
         <div>
-          <label for="productInputCategory">Category :</label>
-          <select name="productInputCategory" id="productInputCategory">
+          <label for="productInputCategory2">Category :</label>
+          <select name="productInputCategory2" id="productInputCategory2">
             <option value="">Select</option>
             <option value="vest" ${
               product.category === "vest" ? "selected" : ""
@@ -328,100 +392,120 @@ for (let btnUpdate of listButtonUpdate) {
         </div>
 
         <div>
-          <label for="productInputInventory">Inventory :</label>
+          <label for="productInputInventory2">Inventory :</label>
           <input
             type="number"
-            name="productInputInventory"
-            id="productInputInventory"
+            name="productInputInventory2"
+            id="productInputInventory2"
             value="${product.inventory}"
           />
         </div>
 
         <div>
-          <label for="productInputImage1">Image 1 :</label>
+          <label for="productInputImage12">Image 1 :</label>
           <input
             type="file"
-            name="productInputImage1"
-            id="productInputImage1"
+            name="productInputImage12"
+            id="productInputImage12"
             accept="image/*"
             value="${product.image1}"
           />
         </div>
 
         <div>
-          <label for="productInputImage2">Image 2 :</label>
+          <label for="productInputImage22">Image 2 :</label>
           <input
             type="file"
-            name="productInputImage2"
-            id="productInputImage2"
+            name="productInputImage22"
+            id="productInputImage22"
             accept="image/*"
             value="${product.image2}"
           />
         </div>
 
         <div class="div-button">
-          <button id="productButtonCreate">Create</button>
+          <button id="productButtonUpdate" title="${product.id}">Update</button>
           <button class="productButtonCancel2">Cancel</button>
         </div>
       </div>`;
+    productButtonUpdate = document.getElementById("productButtonUpdate");
+    productButtonCancel2 = document.querySelector(".productButtonCancel2");
+    productButtonCancel2.addEventListener("click", function () {
+      productFormUpdate.style.display = "none";
+    });
+
+    function updateProduct() {
+      let productInputName2 = document.getElementById("productInputName2");
+      let productInputPrice2 = document.getElementById("productInputPrice2");
+      let productInputColor2 = document.getElementById("productInputColor2");
+      let productInputCategory2 = document.getElementById(
+        "productInputCategory2"
+      );
+      let productInputInventory2 = document.getElementById(
+        "productInputInventory2"
+      );
+      let productInputImage12 = document.getElementById("productInputImage12");
+      let productInputImage22 = document.getElementById("productInputImage22");
+
+      //còn thiếu đoạn code lưu ảnh
+      let imageName1 =
+        productInputImage12 &&
+        productInputImage12.files &&
+        productInputImage12.files[0]
+          ? productInputImage12.files[0].name
+          : `${product.image1}`;
+      let imageName2 =
+        productInputImage22 &&
+        productInputImage22.files &&
+        productInputImage22.files[0]
+          ? productInputImage22.files[0].name
+          : `${product.image2}`;
+
+      let totalProducts = getDataLocalstorage("products");
+      let productName = productInputName2.value;
+      let price = productInputPrice2.value;
+      let color = productInputColor2.value;
+      let category = productInputCategory2.value;
+      let inventory = productInputInventory2.value;
+      let image1 = imageName1;
+      let image2 = imageName2;
+      let id = btnUpdate.getAttribute("title");
+      let index = totalProducts.findIndex((element) => element.id == id);
+
+      let newProduct = {
+        id: id,
+        category: category,
+        name: productName,
+        price: price,
+        image1: image1,
+        image2: image2,
+        color: color,
+        inventory: inventory,
+      };
+
+      totalProducts[index] = newProduct;
+      updateDataLocalStorage("products", totalProducts);
+    }
+    if (productButtonUpdate) {
+      productButtonUpdate.addEventListener("click", function () {
+        updateProduct();
+        alert("Update product successfully !");
+        productFormUpdate.style.display = "none";
+        window.location.reload();
+      });
+    }
   });
 }
-let productButtonCancel2 = document.querySelector(".productButtonCancel2");
 
-console.log("btn cancel 2", productButtonCancel2);
-if (productButtonCancel2) {
-  productButtonCancel2.addEventListener("click", function () {
-    productFormUpdate.style.display = "none";
-  });
-}
+// delete product
 
-if (productButtonUpdate) {
-  productButtonUpdate.addEventListener("click", function () {
-    createNewProduct();
-    alert("Create new product successfully !");
-    productFormCreate.style.display = "none";
+let productButtonDeletes = document.querySelectorAll(".productButtonDelete");
+for (let btnDelete of productButtonDeletes) {
+  btnDelete.addEventListener("click", function () {
+    let id = btnDelete.getAttribute("title");
+    let indexProduct = totalProducts.findIndex((element) => element.id == id);
+    totalProducts.splice(indexProduct, 1);
+    updateDataLocalStorage("products", totalProducts);
     window.location.reload();
   });
-}
-
-// productButtonCancel2.addEventListener("click", function () {
-//   productFormCreate.style.display = "none";
-// });
-
-function createNewProduct() {
-  let productInputName = document.getElementById("productInputName");
-  let productInputPrice = document.getElementById("productInputPrice");
-  let productInputColor = document.getElementById("productInputColor");
-  let productInputCategory = document.getElementById("productInputCategory");
-  let productInputInventory = document.getElementById("productInputInventory");
-  let productInputImage1 = document.getElementById("productInputImage1");
-  let productInputImage2 = document.getElementById("productInputImage2");
-
-  //còn thiếu đoạn code lưu ảnh
-  let imageName1 = productInputImage1.files[0].name;
-  let imageName2 = productInputImage2.files[0].name;
-
-  let totalProducts = getDataLocalstorage("products");
-  let productName = productInputName.value;
-  let price = productInputPrice.value;
-  let color = productInputColor.value;
-  let category = productInputCategory.value;
-  let inventory = productInputInventory.value;
-  let image1 = imageName1;
-  let image2 = imageName2;
-  let id = totalProducts[totalProducts.length - 1].id + 1;
-
-  let newProduct = {
-    id: id,
-    category: category,
-    name: productName,
-    price: price,
-    image1: image1,
-    image2: image2,
-    color: color,
-    inventory: inventory,
-  };
-
-  totalProducts.push(newProduct);
-  updateDataLocalStorage("products", totalProducts);
 }
